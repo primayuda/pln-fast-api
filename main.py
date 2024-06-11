6f17b64d7deaea458f73a2fa617ea05b0235dc69
Aprita Primayuda <aprita.primayuda@gmail.com>
2024-06-11 13:25:24 +0700

    Skeleton end point route

--- main.py
+++ main.py
@@ -4,13 +4,25 @@ app = FastAPI()
 
 message = {
     "status": "success",
-    "message":"null"
+    "message":"root domain"
 }
 
 @app.get("/")
 async def root():
     return message
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
+@app.get("/weather")
+async def weather():
+    return {"message": "weather end point"}
+
+@app.get("/marine")
+async def marine():
+    return {"message": "marine end point"}
+
+@app.get("/coal")
+async def coal():
+    return {"message": "coal end point"}
+
+@app.get("/geopolitics")
+async def geopolitics():
+    return {"message": "geopolitics end point"}
